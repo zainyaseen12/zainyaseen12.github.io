e180f098f1ef9215320edd93d1db7945a0d27231
zainyaseen12 <zaino.yaseen@gmail.com>
2026-09-21 05:42:34 +0500

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -376,3 +376,38 @@
         settings_color();
     });
 })(jQuery);
+
+
+
+/*--custom-csss-*/
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function (e) {
+
+    // F12
+    if (e.key === 'F12') {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl + Shift + I
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl + Shift + J
+    if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'j') {
+        e.preventDefault();
+        return false;
+    }
+
+    // Ctrl + U
+    if (e.ctrlKey && e.key.toLowerCase() === 'u') {
+        e.preventDefault();
+        return false;
+    }
+});
